1157e627b04f07b9634827e01b0a331e89f6a2db
henriquePiresOuriques <henriquepiresouriques@gmail.com>
2020-03-24 15:38:06 -0300

    criação do backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require ('./routes');
 
 const app = express();
 
+app.use(cors(/*
+    *caso seja publicado usar a seguinte sintaxe:
+    {
+        origin: 'nome do site, ex: http://meuapp.com'
+    } 
+    */));
 app.use(express.json());
 app.use(routes);
 
